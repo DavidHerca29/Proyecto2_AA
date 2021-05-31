94da5fda97110cdbb74b05f001989fe90acff343
David <dahercal29@gmail.com>
2021-05-30 23:49:47 -0600

    Correcion de posiciones

--- UI.py
+++ UI.py
@@ -2,28 +2,44 @@ import tkinter
 import turtle
 
 ventana = tkinter.Tk()
-ventana.geometry("600x600")
+ventana.geometry("1200x650")
 ventana.title("Fractal Trees")
+HEIGHT = 650
+WIDTH = 800
 
-canvas = tkinter.Canvas(ventana, height=300, width=400)
-canvas.grid(row=0, columnspan= 6, pady = 3)
+canvas = tkinter.Canvas(ventana)
+canvas.config(width=WIDTH, height=HEIGHT)
+#canvas.grid(row=0, columnspan=1, padx=10, pady=20)
+canvas.pack(side=tkinter.RIGHT)
 
 TurtleSc = turtle.TurtleScreen(canvas)
-maxTugo = turtle.RawTurtle(canvas)
+TurtleSc.tracer(0, 0)
+maxTugo = turtle.RawTurtle(TurtleSc)
+maxTugo.speed(0)
+maxTugo.ht()
+maxTugo.up()
 maxTugo.left(90)
-maxTugo.backward(100)
-maxTugo.speed(400)
+
+maxTugo.backward(HEIGHT*4/9)
+maxTugo.down()
+
+
 def tree(angle, profundidad, grosor, longitud, decremento):
     if profundidad > 0:
         maxTugo.pensize(grosor)
         maxTugo.forward(longitud)
         maxTugo.left(angle)
-        tree(angle, profundidad-1, grosor, longitud*4/5-decremento, decremento)
-        maxTugo.right(angle*2)
-        tree(angle, profundidad-1, grosor, longitud*4/5-decremento, decremento)
+        tree(angle, profundidad - 1, grosor, longitud * 4 / 5 - decremento, decremento)
+        maxTugo.right(angle * 2)
+        tree(angle, profundidad - 1, grosor, longitud * 4 / 5 - decremento, decremento)
         maxTugo.left(angle)
         maxTugo.backward(longitud)
 
-tree(30, 9, 3, 50, 5)
-canvas.pack()
+
+
+
+tree(30, 9, 3, 150, 5)
+
+TurtleSc.update()
+
 ventana.mainloop()
